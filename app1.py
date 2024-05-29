47f2128b15d647da35673603926f21ec2b2b24aa
sryagit <158165193+sryagit@users.noreply.github.com>
2024-05-29 23:13:12 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,69 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 13 02:20:31 2020
-
-@author: Krish Naik
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 15 12:50:04 2020
-
-@author: krish.naik
-"""
-
 
 import numpy as np
 import pickle
 import pandas as pd
-#from flasgger import Swagger
 import streamlit as st 
 
 from PIL import Image
 
-#app=Flask(__name__)
-#Swagger(app)
 
 pickle_in = open("classifier.pkl","rb")
-classifier=pickle.load(pickle_in)
+classifier = pickle.load(pickle_in)
 
-#@app.route('/')
 def welcome():
     return "Welcome All"
 
-#@app.route('/predict',methods=["Get"])
 def predict_note_authentication(variance,skewness,curtosis,entropy):
-    
-    """Let's Authenticate the Banks Note 
-    This is using docstrings for specifications.
-    ---
-    parameters:  
-      - name: variance
-        in: query
-        type: number
-        required: true
-      - name: skewness
-        in: query
-        type: number
-        required: true
-      - name: curtosis
-        in: query
-        type: number
-        required: true
-      - name: entropy
-        in: query
-        type: number
-        required: true
-    responses:
-        200:
-            description: The output values
-        
-    """
    
     prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
     print(prediction)
     return prediction
-
 
 
 def main():
